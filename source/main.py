df7c099d25e245de9a0c22f5478b076d278d4c7a
Amar <amarjashari007@gmail.com>
2021-01-21 16:18:29 +0100

    Adding Hello command  commited by BOOBOO

---
+++ source/main.py
@@ -0,0 +1,23 @@
+import discord
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello! ")
+
+client.run("ODAxNTY3MTg2NDcyMDA5NzQ5.YAijnQ.Q8Suwp7caz0ikIpfwKgaNmkCH6M")
+
+
+
+
+
+
